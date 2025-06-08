63e5349169585dbf45ebea9af90ef6dfa6ed71b2
Young hoon <younghoon@jihyeui-MacBookAir.local>
2025-06-08 20:24:11 +0900

    fix: remove about pages route

--- components/Gnb.vue
+++ components/Gnb.vue
@@ -2,7 +2,6 @@
   <nav class="bg-white border-b border-gray-200 px-4 py-3 flex justify-between items-center">
     <div class="hover:underline text-xl font-bold" @click="router.push('/')">MySite</div>
     <ul class="flex flex-row items-center justify-end gap-6">
-      <li><NuxtLink to="/about" class="hover:underline">About</NuxtLink></li>
       <li><NuxtLink to="/tools" class="hover:underline">Tools</NuxtLink></li>
     </ul>
   </nav>
